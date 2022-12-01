774514fea904713e44a90bd439fd058a360b2302
ViktorRasev <103742526+ViktorRasev@users.noreply.github.com>
2022-12-01 17:30:49 +0100

    Initial commit

---
+++ src/routes/HomeRoute.js
@@ -0,0 +1,20 @@
+function Home() {
+    return (
+      <>
+    <div>TODO Home</div>;
+    <div>TODO Home</div>;
+    <div>TODO Home</div>;
+    <div>TODO Home</div>;
+    <div>TODO Home</div>;
+    <div>TODO Home</div>;
+    <div>TODO Home</div>;
+    <div>TODO Home</div>;
+    <div>TODO Home</div>;
+    <div>TODO Home</div>;
+    <div>TODO Home</div>;
+    <div>TODO Home</div>;
+    </>
+    )
+  }
+  
+  export default Home;
