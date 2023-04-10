c275b26488e6fa312e211ee1f9e34e19970f8d8a
ViktorRasev <103742526+ViktorRasev@users.noreply.github.com>
2023-04-10 21:23:26 +0200

    dropdown menu fixed on mobile fixed

--- src/App.js
+++ src/App.js
@@ -17,14 +17,20 @@ import { NavLink } from "react-bootstrap";
 import RecipeEditedContext from "./RecipeEditedContext";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import styles from "./css/App.module.css"
+
 
 function App() {
   const {isAuthorized, setIsAuthorized}  = useContext(UserContext);
   const [recipeList, setRecipeList] = useState([])
   const [fetchDataState, setFetchDataState] = useState("loading")
+  const [showOffCanvas, setShowOffCanvas] = useState(false)
   const { recipeEdited } = useContext(RecipeEditedContext);
 
   let navigate = useNavigate();
+
+  const handleShowOffCanvas = () => setShowOffCanvas(true)
+  const handleCloseOffCanvas = () => setShowOffCanvas(false)
 
   useEffect(() => {
     const getRecipesData = async () => {
@@ -44,6 +50,23 @@ function App() {
     getRecipesData()
   }, [recipeEdited])
 
+
+  const navigateHome = () => {
+    navigate("/")
+    handleCloseOffCanvas()
+  }
+  const navigateToIngredients = () => {
+    navigate("/ingredientList")
+    handleCloseOffCanvas()
+  }
+  const navigateToRecipe = (recipeId) => {
+    navigate("/recipeDetail?id=" + recipeId)
+    handleCloseOffCanvas()
+  }
+  const switchAuthorizedState = () => {
+    setIsAuthorized(!isAuthorized)
+    handleCloseOffCanvas()
+  }
   function getRecipesListDropdown() {
     if (fetchDataState === "loading") {
       return (
@@ -53,25 +76,19 @@ function App() {
       );
     } else if (fetchDataState === "success") {
       return (
-        <>
-          <NavLink onClick={() => setIsAuthorized(!isAuthorized)}>
-            {isAuthorized ? "Přihlášen" : "Nepřihlášen"}
-          </NavLink>
           <NavDropdown title="Vyberte recept" id="navbarScrollingDropdown">
             {recipeList.map((recipe) => {
               return (
                 <NavDropdown.Item
+                  className={styles.dropdown_item}
                   key={recipe.id}
-                  onClick={() =>
-                    navigate("/recipeDetail?id=" + recipe.id)
-                  }
+                  onClick={() => navigateToRecipe(recipe.id)}
                 >
                   {recipe.name}
                 </NavDropdown.Item>
               );
             })}
           </NavDropdown>
-        </>
       );
     } else if (fetchDataState === "error") {
       return (
@@ -95,19 +112,22 @@ function App() {
       >
         <Container fluid>
           <Nav.Link onClick={() => navigate("/")} style={{ fontSize: "1.5rem" }}>Kucharka</Nav.Link>
-          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-sm`} />
-          <Navbar.Offcanvas id={`offcanvasNavbar-expand-sm`}>
-            <Offcanvas.Header closeButton>
-              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-sm`}>
+          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-sm`} onClick={handleShowOffCanvas}/>
+          <Navbar.Offcanvas id={`offcanvasNavbar-expand-sm`} show={showOffCanvas} >
+            <Offcanvas.Header closeButton onHide={handleCloseOffCanvas}>
+              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-sm`} onClick={navigateHome}>
                 Kucharka
               </Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body>
               <Nav className="justify-content-end flex-grow-1 pe-3">
                 {getRecipesListDropdown()}
-                <Nav.Link onClick={() => navigate("/ingredientList")}>
+                <Nav.Link onClick={navigateToIngredients}>
                   Ingredience
                 </Nav.Link>
+                <NavLink onClick={switchAuthorizedState}>
+                  {isAuthorized ? "Přihlášen" : "Nepřihlášen"}
+                </NavLink>
               </Nav>
             </Offcanvas.Body>
           </Navbar.Offcanvas>
@@ -120,3 +140,7 @@ function App() {
 }
 
 export default App;
+
+
+
+
